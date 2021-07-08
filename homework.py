cba9cecaef6e1685752518b36200e3890cf9be36
Мария Лисицкая <lisitskayamari@gmail.com>
2021-07-08 22:47:45 +0100

    correct mistakes

--- homework.py
+++ homework.py
@@ -2,8 +2,9 @@ import os
 import time
 import logging
 from logging.handlers import RotatingFileHandler
-import requests
 
+import requests
+import json
 from telegram import Bot
 from dotenv import load_dotenv
 
@@ -13,56 +14,82 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-HW_STATUSES_URL = \
-    'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+HOMEWORK_STATUSES_URL = ('https://praktikum.yandex.ru/'
+                         'api/user_api/homework_statuses/')
 UNEXPECTED_STATUS = 'Статус работы неизвестен.'
-PROJECT_CHECKED = ('У вас проверили работу \"{homework_name}\"!\n\n{verdict}')
-HOMEWORKS_KEY = 'homeworks'
+PROJECT_CHECKED = ('У вас проверили работу \"{name}\"!\n\n{verdict}')
 
-bot = Bot(token=TELEGRAM_TOKEN)
+BOT = Bot(token=TELEGRAM_TOKEN)
 
-bot_dict = {'reviewing': 'Проект находится на ревью.',
+VERDICTS = {'reviewing': 'Проект находится на ревью.',
             'rejected': 'К сожалению, в работе нашлись ошибки.',
             'approved': 'Ревьюеру всё понравилось, работа зачтена!'}
 
 
 def parse_homework_status(homework):
-    homework_status = homework['status']
-    if homework_status is None or homework_status not in bot_dict:
-        raise UNEXPECTED_STATUS
-    verdict = bot_dict[homework_status]
+    status = homework['status']
+    if status not in VERDICTS:
+        raise ValueError(f'Обнаружен неподдерживаемый статус: {status}')
+    verdict = VERDICTS[status]
     return PROJECT_CHECKED.format(
         verdict=verdict,
-        homework_name=homework['homework_name']
+        name=homework['homework_name']
     )
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses_url = HW_STATUSES_URL
-    headers = HEADERS
+    homework_statuses_url = HOMEWORK_STATUSES_URL
     payload = {'from_date': current_timestamp}
     try:
         response = requests.get(
             homework_statuses_url,
-            headers=headers,
+            headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'},
             params=payload
-        ).json()
+        )
     except requests.exceptions.RequestException as error:
-        raise f'Ошибка в ответе от сервера: {error}'
-    except TypeError as error:
-        raise f'Ошибка в при распаковке ответа от сервера: {error}'
-    except ValueError as error:
-        raise f'Ошибка в при распаковке ответа от сервера: {error}'
-    return response
+        raise AssertionError(
+            f'Ошибка в ответе от сервера: {error}.'
+            f'Код: {error.response.status_code}'
+            f'url: {error.request.url}'
+            f'headers: {error.request.headers}'
+            f'params: {error.request.params}'
+        )
+    try:
+        json_response = response.json()
+    except json.JSONDecodeError:
+        raise ValueError(
+            f'Ошибка в при распаковке ответа от сервера. Response: {response}'
+        )
+    return json_response
 
 
 def send_message(message):
-    return bot.send_message(CHAT_ID, message)
+    return BOT.send_message(CHAT_ID, message)
 
 
 def main():
-    timestamp = int(time.time())
+
+    while True:
+        timestamp = int(time.time())
+        try:
+            homeworks_statuses = get_homeworks(timestamp)
+            if 'homeworks' in homeworks_statuses:
+                homeworks = homeworks_statuses['homeworks']
+                if len(homeworks) > 0:
+                    message = parse_homework_status(homeworks[0])
+                    send_message(message)
+            time.sleep(30 * 60)
+
+        except Exception as error:
+            logging.error(
+                msg='Не удалось выполнить итерацию. '
+                    f'Ошибка: {error}',
+                exc_info=True
+            )
+            time.sleep(30)
+
+
+if __name__ == '__main__':
     logging.basicConfig(
         level=logging.DEBUG,
         filename=__file__ + '.log',
@@ -73,25 +100,7 @@ def main():
     handler = RotatingFileHandler(
         'my_logger.log',
         maxBytes=50000000,
-        backupCount=5)
+        backupCount=5
+    )
     logger.addHandler(handler)
-
-    while True:
-        try:
-            homeworks_statuses = get_homeworks(timestamp)
-            if HOMEWORKS_KEY not in homeworks_statuses:
-                raise UNEXPECTED_STATUS
-            homeworks = homeworks_statuses[HOMEWORKS_KEY]
-            if len(homeworks) > 0:
-                message = parse_homework_status(homeworks[0])
-            if message is not None:
-                send_message(message)
-            time.sleep(30 * 60)
-
-        except Exception as error:
-            logging.error(error, exc_info=True)
-            time.sleep(30)
-
-
-if __name__ == '__main__':
     main()
